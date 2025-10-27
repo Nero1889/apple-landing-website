4c246aa717b115c6158844bb8b7e96cf3dda41a0
Alexis Luna <mceclipse1889@gmail.com>
2025-10-26 22:29:27 -0500

    Update ProductViewer.jsx

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,20 +1,41 @@
+import clsx from "clsx";
+import useMacbookStore from "../store/index.js";
+
 function ProductViewer() {
+    const {color, scale, setColor, setScale} = useMacbookStore();
+
     return (
         <section id="product-viewer">
             <h2>Take a closer look.</h2>
 
             <div className="controls">
-                <p className="info">Macbook Pro 16" Space Black</p>
+                <p className="info">Macbook Pro 16" in {color}</p>
 
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
-                        <div className="bg-neutral-300"/>
-                        <div className="bg-neutral-900"/>
+                        <div onClick={() => setColor("#adb5bd")}
+                        className={clsx("bg-neutral-50",
+                        color === "#adb5bd" && "active")}/>
+
+                        <div onClick={() => setColor("#2e2c2e")}
+                        className={clsx("bg-neutral-900",
+                        color === "#2e2c2e" && "active")}/>
                     </div>
 
                     <div className="size-control">
-                        <div><p>14"</p></div>
-                        <div><p>16"</p></div>
+                        <div onClick={() => setScale(0.06)}
+                        className={clsx(scale === 0.06 
+                        ? "bg-white text-black"
+                        : "bg-transparent text-white")}>
+                            <p>14"</p>
+                        </div>
+
+                        <div onClick={() => setScale(0.08)}
+                        className={clsx(scale === 0.08 
+                        ? "bg-white text-black"
+                        : "bg-transparent text-white")}>
+                            <p>16"</p>
+                        </div>
                     </div>
                 </div>
             </div>
