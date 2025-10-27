306fa1f32ec31ce72c55cd9bb746152fb93409b3
Alexis Luna <mceclipse1889@gmail.com>
2025-10-26 19:35:50 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "./style.css";
+import NavBar from "./components/NavBar.jsx";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <NavBar/>
+    </StrictMode>,
+);
