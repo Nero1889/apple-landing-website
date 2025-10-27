d3f6066d4e2a32652d594457ac1768b4681f316b
Alexis Luna <mceclipse1889@gmail.com>
2025-10-26 22:56:56 -0500

    Update ProductViewer.jsx

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import useMacbookStore from "../store/index.js";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14.jsx";
 
 function ProductViewer() {
     const {color, scale, setColor, setScale} = useMacbookStore();
@@ -42,9 +43,10 @@ function ProductViewer() {
                 </div>
             </div>
 
-            <Canvas id="canvas" camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
-                <Box position={[0, 0, 0]} scale={10 * scale} material-color={color}></Box>
-
+            <Canvas id="canvas" camera={{position: [0, 2, 5], fov: 50, near: 0.1,
+            far: 100}}>
+                <ambientLight intensity={0.5}/>
+                <MacbookModel14 scale={0.06} position={[0, 0, 0]}/>
                 <OrbitControls enableZoom={false}/>
             </Canvas>
         </section>
