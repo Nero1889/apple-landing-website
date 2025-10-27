314da7ad2df5045e0e373087f3e4446c764a7cd5
Alexis Luna <mceclipse1889@gmail.com>
2025-10-26 23:07:48 -0500

    Update ProductViewer.jsx

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,6 +3,7 @@ import useMacbookStore from "../store/index.js";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14.jsx";
+import StudioLights from "./StudioLights.jsx";
 
 function ProductViewer() {
     const {color, scale, setColor, setScale} = useMacbookStore();
@@ -10,10 +11,8 @@ function ProductViewer() {
     return (
         <section id="product-viewer">
             <h2>Take a closer look.</h2>
-
             <div className="controls">
                 <p className="info">Macbook Pro 16" in {color}</p>
-
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
                         <div onClick={() => setColor("#adb5bd")}
@@ -24,7 +23,6 @@ function ProductViewer() {
                         className={clsx("bg-neutral-900",
                         color === "#2e2c2e" && "active")}/>
                     </div>
-
                     <div className="size-control">
                         <div onClick={() => setScale(0.06)}
                         className={clsx(scale === 0.06 
@@ -32,7 +30,6 @@ function ProductViewer() {
                         : "bg-transparent text-white")}>
                             <p>14"</p>
                         </div>
-
                         <div onClick={() => setScale(0.08)}
                         className={clsx(scale === 0.08 
                         ? "bg-white text-black"
@@ -42,10 +39,9 @@ function ProductViewer() {
                     </div>
                 </div>
             </div>
-
             <Canvas id="canvas" camera={{position: [0, 2, 5], fov: 50, near: 0.1,
             far: 100}}>
-                <ambientLight intensity={0.5}/>
+                <StudioLights/>
                 <MacbookModel14 scale={0.06} position={[0, 0, 0]}/>
                 <OrbitControls enableZoom={false}/>
             </Canvas>
