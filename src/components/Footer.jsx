3cca4823cb9977bd822dc4eb107ab0dd85b7b2a1
Alexis Luna <mceclipse1889@gmail.com>
2025-10-28 14:43:41 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
+import {footerLinks} from "../constants/index.js";
+
 function Footer() {
     return (
         <footer id="footer">
-            d
+            <div className="info">
+                <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 0008000 040 1966</p>
+                <img src="/logo.svg" alt="Apple Logo"/>
+            </div>
+
+            <hr/>
+            <div className="links">
+                <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+                <ul>
+                    {footerLinks.map(({label, link}) => {
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    })}
+                </ul>
+            </div>
         </footer>
     );
 }
