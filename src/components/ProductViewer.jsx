77e8fd5104db1d6a934fb2483a9a33fa72903ef4
Alexis Luna <mceclipse1889@gmail.com>
2025-10-30 00:05:19 -0500

    Update ProductViewer.jsx

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -16,7 +16,8 @@ function ProductViewer() {
         <section id="product-viewer">
             <h2>Take a closer look.</h2>
             <div className="controls">
-                <p className="info">Macbook Pro | Available in 14" & 16" in Space Gray & Dark Colors</p>
+                <p className="info">Macbook Pro | Available in 14" & 16" in Space Gray &
+                Dark Colors</p>
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
                         <div onClick={() => setColor("#adb5bd")}
@@ -27,6 +28,7 @@ function ProductViewer() {
                         className={clsx("bg-neutral-900",
                         color === "#2e2c2e" && "active")}/>
                     </div>
+
                     <div className="size-control">
                         <div onClick={() => setScale(0.06)}
                         className={clsx(scale === 0.06 
@@ -34,6 +36,7 @@ function ProductViewer() {
                         : "bg-transparent text-white")}>
                             <p>14"</p>
                         </div>
+
                         <div onClick={() => setScale(0.08)}
                         className={clsx(scale === 0.08 
                         ? "bg-white text-black"
@@ -46,7 +49,8 @@ function ProductViewer() {
             <Canvas id="canvas" camera={{position: [0, 2, 5], fov: 50, near: 0.1,
             far: 100}}>
                 <StudioLights/>
-                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile}/>
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale}
+                isMobile={isMobile}/>
             </Canvas>
         </section>
     );
